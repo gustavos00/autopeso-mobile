d80a190f25c4ed545624d5380a14518a8e53669e
Gustavo Santos <z.gsantos20@gmail.com>
2022-12-17 14:41:14 -0100

    Apply eslint and prettier configurations

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,25 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  semi: true,
+  trailingComma: 'none',
   singleQuote: true,
-  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  importOrder: [
+    // Place angular import at the top
+    '^@angular$',
+    'rxjs',
+    // External dependencies
+    '^w',
+    // This project's own aliases
+    '^(@shared|@core|@env|@test-utilities)(/.*|$)',
+    // This project's extra libraries
+    '^(business-logic|@ui)(/.*|$)',
+    // Every import starting with ./ or ~/
+    '^[./|~/]'
+  ],
+  importOrderSeparation: true,
+  importOrderParserPlugins: ['decorators-legacy', 'typescript']
 };
